cfdd873b4568f180e6f37db318b9fb52604aa6cf
Hein Htet <arjunyadav.hash@gmail.com>
2025-09-20 12:28:46 +0630

    feat(project): implement project page

--- app/(dashboard)/projects/[id]/page.tsx
+++ app/(dashboard)/projects/[id]/page.tsx
@@ -1,5 +1,34 @@
-const ProjectDetailPage = () => {
-	return <h1>Project Detail Page</h1>;
+import TaskCard from "@/components/TaskCard";
+import { delay } from "@/lib/async";
+import { getUserFromCookie } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { cookies } from "next/headers";
+
+const getData = async (id: string) => {
+	await delay(3000);
+
+	const user = await getUserFromCookie(cookies());
+
+	const projects = await db.project.findFirst({
+		where: {
+			id,
+			ownerId: user?.id
+		},
+		include: {
+			tasks: true
+		}
+	});
+
+	return { projects };
+};
+const ProjectPage = async ({ params }: { params: { id: string } }) => {
+	const { projects } = await getData(params.id);
+
+	return (
+		<div className="h-full overflow-y-auto pr-6 w-full">
+			<TaskCard tasks={projects?.tasks} title={projects?.name || ""} />
+		</div>
+	);
 };
 
-export default ProjectDetailPage;
+export default ProjectPage;
